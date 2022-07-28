3920a8216b3cbc05ebeeec82216cd59c4c8ff0fa
Knn <w19951129326@yahoo.com.tw>
2022-07-28 19:09:20 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,14 @@ base: '/<REPO>/'
 npm run build
 git add dist -f
 git commit -m "adding dist"
-git subtree push --prefix dist origin gh-pages 可以自行設定名稱
-
+git subtree push --prefix dist origin gh-pages 可以自行設定名稱 --失敗
+git push -f git@github.com:Knn84/<REPO>.git main:gh-pages 可以自行設定名稱
 # 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 git push -f https://knn84.github.io/vite-test.git 
-git push -f git@github.com:Knn84/vite-test.git master:gh-pages
+git push -f git@github.com:Knn84/vue3-bootstrap5-test.git main:gh-pages
 
+cd -
